f7a90da6c441942df155d7b7041c6778f24d043f
Chris Towles <Chris.Towles@gmail.com>
2023-02-26 22:32:41 -0500

    chore: update cli

---
+++ cli/readme.md
@@ -0,0 +1,12 @@
+# CLI
+
+Register as global command.
+
+```bash
+pnpm link -g
+```
+
+
+```
+
+towles-cli
--- cli/src/config.ts
+++ cli/src/config.ts
@@ -1,5 +1,5 @@
 // import _debug from 'debug'
 
-// const debug = _debug('towles:config')
+// const debug = _debug('towles-cli:config')
 
 export const LOGLEVELS = ['debug', 'info', 'warn', 'error', 'silent']
