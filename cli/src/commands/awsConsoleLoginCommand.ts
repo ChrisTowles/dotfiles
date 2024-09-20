3068f63e617ff793beb86cd0d7ff337d7a452dad
Chris Towles <Chris.Towles@gmail.com>
2024-09-19 21:35:18 -0400

    chore: aws login to console working

--- cli/src/commands/awsConsoleLoginCommand.ts
+++ cli/src/commands/awsConsoleLoginCommand.ts
@@ -1,31 +1,58 @@
-import { type Command } from 'commander'
+import { type Command, Argument } from 'commander'
 import c from 'picocolors'
-import { } from '@aws-sdk/credential-provider-ini'
+import { fromIni } from '@aws-sdk/credential-provider-ini'
+import querystring from 'node:querystring'
+import { ofetch } from "ofetch";
+import open from "open";
 
 
 export const SetupAwsConsoleLoginCommand = (program: Command): void => {
   program.command('aws-console')
     .description('use aws cli permissions to open aws console')
+    .argument('[profile]', 'aws profile name', 'default')
     .action(async (args) => {
 
-      console.log(c.blue('Setting up AWS Console Login...' + client))
-      // const mainBranchName = await getDefaultMainBranchName()
-      // console.log(`Main Branch Name: ${mainBranchName}`)
-      // const alreadyMergedBranches = await getMergedBranches(mainBranchName)
 
-      // if (alreadyMergedBranches.length === 0) {
-      //   console.log(c.yellow('No branches found to cleanup'))
-      //   process.exit(0)
-      // }
+      try {
+        const identity = await fromIni(args.profile)()
 
-      // console.log(`Found ${c.green(alreadyMergedBranches.length)} branches that have already been merged to ${mainBranchName}.`)
-      // console.log('')
-      // console.log(c.yellow('Run the following commands to delete these branches:'))
-      // console.log('')
-      // // TODO: add a flag to actually delete the branches
-      // alreadyMergedBranches.forEach((branch) => {
-      //   console.log(`git branch -d ${branch}`)
-      // })
+        console.log(c.blue(`Setting up AWS Console Login for profile: ${args.profile}`));
+
+        const url_credentials = {
+          sessionId: identity.accessKeyId,
+          sessionKey: identity.secretAccessKey,
+          sessionToken: identity.sessionToken
+        }
+
+        const urlPrameters = {
+          'Action': 'getSigninToken',
+          'DurationSeconds': 43200,
+          'Session': JSON.stringify(url_credentials)
+        }
+
+        const request_url = "https://signin.aws.amazon.com/federation?" + querystring.encode(urlPrameters)
+        const response = await ofetch(request_url, { parseResponse: JSON.parse })
+
+        console.log(c.blue(`succesful got sign In Token`))
+
+
+        const loginUrlParams = {
+          'Action': 'login',
+          'Destination': 'https://console.aws.amazon.com/',
+          'SigninToken': response.SigninToken,
+          'Issuer': 'https://example.com'
+        }
+
+        const loginUrl = "https://signin.aws.amazon.com/federation?" + querystring.encode(loginUrlParams);
+
+        console.log(c.yellow('Opening Url in Browser'))
+        await open(loginUrl, { wait: false });
+
+      } catch (error) {
+        console.error(c.red('Error trying to login with AWS credentials'));
+        process.exit(1);
+      }
+
 
       console.log('')
       console.log(c.green('Done!'))
